5f0e900690a0b38acd61f44a9ba144ca93e1ce8c
Jesse Fischer <jesse@jessefischer.com>
2023-11-16 15:57:43 -0500

    feat: design updates

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       </div>
       <Grid>
         <BoxLink title="Koch Synth" href="https://koch-synth.netlify.app/" alt="Koch Synth" imgSrc="assets/koch-synth.png" />
-        <BoxLink title="Rubik's Snake" href="https://snake-sequencer.netlify.app/" alt="Rubik's Snake" imgSrc="assets/snake-sequencer.png" />
+        <BoxLink title="Rubik’s Snake" href="https://snake-sequencer.netlify.app/" alt="Rubik's Snake" imgSrc="assets/snake-sequencer.png" />
         <BoxLink title="Three Golden Rectangles" href="https://three-golden-rectangles.netlify.app/" alt="Three Golden Rectangles" imgSrc="assets/three-golden-rectangles.png" />
         <BoxLink title="Arp Tesselator" href="https://jessefischer.github.io/arptesselator/" alt="Arp Tesselator" imgSrc="assets/arp-tesselator.png" />
         <BoxLink title="Dots and Boxes" href="https://jf-dots-and-boxes.netlify.app/" alt="Dots and Boxes" imgSrc="assets/dots-and-boxes.png" />
